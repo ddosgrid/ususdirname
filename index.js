282153ef274b6976e108caa0dd2b649542863e2c
Kyrill <realChesta@users.noreply.github.com>
2022-01-27 18:35:10 +0100

    add and use dirname parameter

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { fileURLToPath } from 'url'
 import { dirname as path_dirname } from 'path'
 
-export default function dirname () {
-  var __filename = fileURLToPath(import.meta.url)
+export default function dirname (metaUrl) {
+  var __filename = fileURLToPath(metaUrl || import.meta.url)
   var __dirname = path_dirname(__filename)
   return __dirname
 }
